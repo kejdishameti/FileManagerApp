87a4a172deb147df8f596cbc034a415f7a45d022
Kejdi <kejdishameti@gmail.com>
2025-01-14 12:13:19 +0100

    Add FolderTreeDTO

---
+++ FileManagerApp.API/DTO/Folder/FolderTreeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FileManagerApp.API.DTO.Folder
+{
+    public class FolderTreeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<FolderTreeDTO> Children { get; set; } = new List<FolderTreeDTO>();
+    }
+}
